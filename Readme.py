44b543636709b1014ba1fbe389511f519eb067db
4Fortune8 <59087629+4Fortune8@users.noreply.github.com>
2025-01-26 11:50:20 -0600

    Update Readme.py

--- Readme.py
+++ Readme.py
@@ -1,1 +1,4 @@
 An ESP32 project that allows for activity in twitch, youtube, and kik chat to drive activity on a led setup desiged to react with chat run entirely on the esp32 chip
+
+
+works but is a bit of a mess as this project is currently dead
